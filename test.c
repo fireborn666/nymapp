19d95b0156dd5903eaffc6bcc5c2a1531a56ec7b
Dmitri Mikhalev <dmitri.mikhalev@ha.ax>
2022-12-21 09:06:51 +0200

    tog bort allt och la till första raden

--- test.c
+++ test.c
@@ -1,9 +1,1 @@
-git add test.c
-
-en modifikation
-git remote add origin https://github.com/fireborn666/nymapp.git
-
-en lite nförändring
-
-bla bla __BIGGEST_ALIGNMENT__
-osv tra la __USER_LABEL_PREFIX__
+#include <stdio.h>
